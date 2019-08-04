a5d8d53ecc8330db342484a0cfbaa47404988cb9
Manisha Khatri <manishakhatri756@gmail.com>
2019-08-04 12:34:56 +0530

    Applied MVP architecture

---
+++ app/src/main/java/com/example/newsanytime/contract/HomeActivityContract.java
@@ -0,0 +1,8 @@
+package com.example.newsanytime.contract;
+
+import com.example.newsanytime.pojo.News;
+
+public interface HomeActivityContract {
+
+    public void showTopHeadlinesList(News news);
+}
